686a1e7299e19d64833c9bca5086efcc42ad3d96
Maksym Baldyniuk <frundzik1989@gmail.com>
2024-12-12 20:27:19 +0200

    feat: resolved to countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -242,8 +242,12 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const minToString = String(minutes);
+  const secToString = String(seconds);
+  const mm = minToString.padStart(2, '0');
+  const ss = secToString.padStart(2, '0');
+  return `${mm}:${ss}`;
 }
 
 /**
@@ -256,8 +260,12 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    result += str.at(i);
+  }
+  return result;
 }
 
 /**
@@ -271,8 +279,10 @@ function reverseString(/* str */) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  const array = str.split('');
+  const sort = array.sort();
+  return sort.join('');
 }
 
 /**
@@ -287,8 +297,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -305,8 +315,14 @@ function containsSubstring(/* str, substring */) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  let result = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  for (let i = 0; i <= str.length - 1; i += 1) {
+    const subStr = str[i];
+    if (vowels.includes(subStr)) result += 1;
+  }
+  return result;
 }
 
 /**
